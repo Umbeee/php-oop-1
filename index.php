069b5c130fe453859bd29e3ca54c99dbf4d0ec1d
Umbeee <umberto.nesta@hotmail.com>
2023-05-31 18:10:15 +0200

    tabella completa, rimane da stampare risultato funzione getEtaFilm

--- index.php
+++ index.php
@@ -6,9 +6,6 @@ Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d'istan
    => all'interno della classe è definito almeno un metodo
 - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà  -->
 
-
-
-
 <?php
 
     class Movie {
@@ -19,6 +16,7 @@ Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d'istan
         public $director;
         public $mainActor;
         public $year;
+        public $stringEta = '';
         // public $awards;
 
        function __construct($_name, $_category, $_lenght, $_director, $_mainActor, $_year){
@@ -29,12 +27,31 @@ Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d'istan
             $this->mainActor = $_mainActor;
             $this->year = $_year;
        }
+
+       public function getEtaFilm( $anno ) {
+
+          if( $anno > '2003' ){
+                $this->stringEta = "Il Film ha meno di vent'anni";
+          }else{
+                $this->stringEta = "Il Film ha almeno vent'anni";
+          }
+
+       }
+
     }
 
-    $pulpfiction = new Movie('Pulpfiction', 'crime', '154 min', 'Quentin Tarantino', 'John Travolta', '1994' );
+    $pulpfiction = new Movie('Pulpfiction', 'crime', '154 min', 'Quentin Tarantino', 'John Travolta', 1994 );
     
-    $interstellar = new Movie( 'Interstellar', 'sci-fi', '169 min', 'Christopher Nolan', 'Matthew McConaughey', '2014' );
 
+    $interstellar = new Movie( 'Interstellar', 'sci-fi', '169 min', 'Christopher Nolan', 'Matthew McConaughey', 2004 );
+    
+
+    $arrayFilm = [
+        new Movie('Pulpfiction', 'crime', '154 min', 'Quentin Tarantino', 'John Travolta', 1994 ),
+        new Movie( 'Interstellar', 'sci-fi', '169 min', 'Christopher Nolan', 'Matthew McConaughey', 2004 )
+    ];
+    $pulpfiction->getEtaFilm('1994');
+    $interstellar->getEtaFilm('2004');
     var_dump($pulpfiction, $interstellar);
 ?>
 
@@ -45,9 +62,61 @@ Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d'istan
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>php oop 1</title>
 </head>
+<style>
+    .container{
+        margin-top: 10em;
+    }
+</style>
 <body>
     
+    <div class="container">
+
+        <h1 class="text-center my-5">Tabella Film</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    
+                    <th>name</th>
+                    <th>category</th>
+                    <th>lenght</th>
+                    <th>director</th>
+                    <th>main actor</th>
+                    <th>year</th>
+                </tr>
+            </thead>
+            <tbody>
+               
+                    
+                    <?php  foreach ( $arrayFilm as $elem ) { ?>
+                        <tr>
+                            
+                            <td><b><?php echo $elem->name  ?> </b> </td>
+                            <td> <?php echo $elem->category  ?> </td>
+                            <td> <?php echo $elem->lenght  ?> </td>
+                            <td> <?php echo $elem->director  ?> </td>
+                            <td> <?php echo $elem->mainActor  ?> </td>
+                            <td> <?php echo $elem->year  ?> </td>
+                        </tr>
+                    <?php } ?>
+                    
+            
+            </tbody>
+        </table>
+
+        <div>
+            <?php  foreach ( $arrayFilm as $elem ) { ?>
+                <p><?php echo $elem->stringEta ?></p>
+            <?php } ?>
+        </div>
+
+    </div>
+
+    
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
